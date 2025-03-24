e6062df20ff0f0b75e53ac8417096f62e6ee0396
Hanas <53557422+hanasoke@users.noreply.github.com>
2025-03-24 14:28:17 +0700

    adding put/patch on Motorcycle

--- application/controllers/api/Vga_cards.php
+++ application/controllers/api/Vga_cards.php
@@ -28,7 +28,27 @@ class Vga_cards extends CI_Controller {
     public function detail($id) {
 
         // Create a DateTime object with the GMT+7 timezone
-        
+        $date = new DateTime('now', new DateTimeZone('Asia/Jakarta'));
+
+        // Format the date and time
+        $update_date = $date->format('Y-m-d H:i:s');
+
+        $method = $this->input->method(); // Get HTTP method (get, put, patch, delete)
+
+        // Get: Vga Card
+        if($method === 'get') {
+            $vga_card = $this->db->get_where('vga_cards', ['id_card' => $id])->row();
+
+            if($vga_card) {
+                $this->output 
+                    ->set_content_type('application/json')
+                    ->set_output(json_encode($vga_card));
+            } else {
+                $this->output 
+                    ->set_status_header(404)
+                    ->set_output(json_encode(['error' => 'Vga Card not found']));
+            }
+        }
     }
 
     
