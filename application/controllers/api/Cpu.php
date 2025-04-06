019575f53fcf1fad83033a11c0d7e0558f446767
Hanas <53557422+hanasoke@users.noreply.github.com>
2025-04-06 21:30:39 +0700

    update Cpu Controller

--- application/controllers/api/Cpu.php
+++ application/controllers/api/Cpu.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cpu extends CI_Controller {
 
     public function __construct() {
-        parent::construct();
+        parent::__construct();
         // Load any necessary libraries or helpers
         $this->load->helper('url');
         $this->load->library('form_validation');
@@ -28,9 +28,51 @@ class Cpu extends CI_Controller {
     // Example: Get a single cpus by ID
     public function detail($id) {
 
-        $method = $this->input->method(); // Get HTTP method (get, put, patch, delete) 
+        $method = $this->input->method(); // Get HTTP method (get & delete) 
 
-        
+        // Get: Cpu 
+        if ($method === 'get') {
+            $cpu = $this->db->get_where('cpus', ['cpu_id' => $id])->row();
+
+            if ($cpu) {
+                $this->output 
+                    ->set_content_type('application/json')
+                    ->set_output(json_encode($cpu));
+            } else {
+                $this->output 
+                    ->set_status_header(404)
+                    ->set_output(json_encode(['error' => 'Cpu not found']));
+            }
+
+        }
+
+        // Delete cpu 
+        elseif ($method === 'delete') {
+            // Check if cpu exists
+        }
+
+        // Handle Invalid methods
+        else {
+            $this->output 
+                ->set_status_header(405)
+                ->set_content_type('application/json')
+                ->set_output(json_encode(['error' => 'Method Not Allowed']));
+            return;
+        }
+    }
+
+    // Create a new Cpu 
+    public function add() {
+        // Create a DateTime object with the GMT+7 timezone
+        $date = new DateTime('now', new DateTimeZone('Asia/Jakarta'));
+
+        // Format the date and time
+        $created_date = $date->format('Y-m-d H:i:s');
+
+        // get and convert price first 
+        $price = $this->input->post('price');
+        $price = preg_replace('/[^0-9]/', '', $price);
+        $price = (int)$price;
 
     }
 
