25c3b8bc1a316846d919e7ba6bd5dc88a6085ba6
Hanas <53557422+hanasoke@users.noreply.github.com>
2025-04-12 11:35:23 +0700

    adding Computer Controller

---
+++ application/controllers/api/Computer.php
@@ -0,0 +1,21 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Computer extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        // Load any necessary libraries or helpers
+        $this->load->helper('url');
+        $this->load->library('form_validation', 'upload');
+        $this->load->database();
+    }
+
+    // Example: Get all Computer 
+    public function all() {
+        
+    }
+
+}
+
+?>
